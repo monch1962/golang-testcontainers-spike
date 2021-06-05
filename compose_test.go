c8ab26012a7ded16edaa8608378cbd204d469295
David Mitchell <monch1962@gmail.com>
2021-06-05 12:13:09 +0000

    Updates

--- compose_test.go
+++ compose_test.go
@@ -6,11 +6,34 @@ import (
 	//"time"
 	"strings"
 	"github.com/google/uuid"
+	//"sync"
 
 	"github.com/testcontainers/testcontainers-go"
 	//"github.com/testcontainers/testcontainers-go/wait"
 )
 
+/*
+// TODO: Set up the Docker Compose test env as a singleton. That way you 
+// can call GetTestEnv() at the start of each test case, and DestroyTestEnv()
+// at the end of some test cases - only if the test env has been compromised by
+// executing the test case
+type TestEnvDetail struct {
+	compose testcontainers
+}
+var testEnv *TestEnvDetail
+
+func GetTestEnv() *TestEnvDetail {
+    sync.Once.Do(func() {
+        testEnv = &TestEnvDetail{state: "off"}
+    })
+    return testEnv
+}
+
+func DestroyTestEnv() *TestEnvDetail {
+	execError := compose.Down()
+	return execError, compose
+}
+*/
 
 func TestWithCompose(t *testing.T) {
 	//ctx := context.Background()
